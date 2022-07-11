0888a60966895aa3a584e9ebc47ed73727229605
Grushko Stefan <anion0278@gmail.com>
2022-07-11 20:41:43 +0200

    Double-clicking items in datagrid opens editing

--- BleRecorder.UI.WPF/View/TestSubjectDetailView.xaml.cs
+++ BleRecorder.UI.WPF/View/TestSubjectDetailView.xaml.cs
@@ -1,15 +1,27 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
+using BleRecorder.UI.WPF.ViewModels;
 
 namespace BleRecorder.UI.WPF.View
 {
-  /// <summary>
-  /// Interaction logic for testSubjectDetailView.xaml
-  /// </summary>
-  public partial class testSubjectDetailView : UserControl
-  {
-    public testSubjectDetailView()
+    /// <summary>
+    /// Interaction logic for testSubjectDetailView.xaml
+    /// </summary>
+    public partial class testSubjectDetailView : UserControl
     {
-      InitializeComponent();
+        public testSubjectDetailView()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Row_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // fast hack that works well. Its UI responsibility anyway
+            var viewModel = (TestSubjectDetailViewModel)DataContext;
+            if (viewModel.EditMeasurementCommand.CanExecute(null))
+                viewModel.EditMeasurementCommand.Execute(null);
+
+        }
     }
-  }
 }
