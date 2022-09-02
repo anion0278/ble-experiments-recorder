557e7b085ef93f947194b2c691a823f7dab7f019
Grushko Stefan <anion0278@gmail.com>
2022-09-02 12:21:12 +0200

    Added blur barier for graph when not enough data are available fixed AB#28

---
+++ BleRecorder.UI.WPF/Views/Resouces/MeasuredDataToBooleanConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using Castle.Core.Internal;
+using BleRecorder.UI.WPF.ViewModels;
+
+namespace BleRecorder.UI.WPF.Views.Resouces;
+
+[ValueConversion(typeof(IEnumerable), typeof(bool))]
+public class MeasuredDataToBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        if (targetType != typeof(bool) && targetType != typeof(object))
+            throw new InvalidOperationException("The target must be a boolean");
+
+        return ((IEnumerable)value).IsNullOrEmpty();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
+
+
+public class MultiCollectionToVisibilityConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        var collections = values as IList<object>;
+        if (collections.First() == DependencyProperty.UnsetValue) return Visibility.Collapsed;
+        return collections.Cast<ICollection>().Select(collection => collection.Count).All(c => c >= 1) ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BleRecorder.UI.WPF/Views/Resouces/TabControlPreventVirtualizationBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Threading;
+using Microsoft.Xaml.Behaviors;
+
+namespace BleRecorder.UI.WPF.Views.Resouces;
+
+// wraps each tab into user control to avoid virtualization
+public class TabControlPreventVirtualizationBehavior : Behavior<UIElement>
+{
+    private TabControl AssociatedTabControl { get { return (TabControl)AssociatedObject; } }
+
+    protected override void OnAttached()
+    {
+        ((INotifyCollectionChanged)AssociatedTabControl.Items).CollectionChanged += TabControlUcWrapperBehavior_CollectionChanged;
+        base.OnAttached();
+    }
+
+    protected override void OnDetaching()
+    {
+        ((INotifyCollectionChanged)AssociatedTabControl.Items).CollectionChanged -= TabControlUcWrapperBehavior_CollectionChanged;
+        base.OnDetaching();
+    }
+
+    void TabControlUcWrapperBehavior_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action != NotifyCollectionChangedAction.Add)
+            return;
+
+        foreach (var newItem in e.NewItems)
+        {
+            var ti = AssociatedTabControl.ItemContainerGenerator.ContainerFromItem(newItem) as TabItem;
+
+            if (ti != null && !(ti.Content is UserControl))
+                ti.Content = new UserControl { Content = ti.Content };
+        }
+    }
+}
