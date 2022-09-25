21f8de9a911730b753756ffb50de8428f348a155
Grushko Stefan <anion0278@gmail.com>
2022-09-25 16:24:33 +0200

    solved AB#31

--- BleRecorder.Models/Device/DeviceCalibration.cs
+++ BleRecorder.Models/Device/DeviceCalibration.cs
@@ -1,21 +1,30 @@
 ﻿using System.ComponentModel;
+using System.Runtime.Serialization;
 
 namespace BleRecorder.Models.Device;
 
 public class DeviceCalibration
 {
-    private const double DefaultParametersValue = 1;
+    private const double DefaultParametersValue = 0;
 
     public double NoLoadSensorValue { get; set; }
     public double NominalLoadSensorValue { get; set; }
+    public double NominalLoad { get; set; }
 
     public static DeviceCalibration GetDefaultValues() 
     {
-        return new DeviceCalibration() { NoLoadSensorValue = 1, NominalLoadSensorValue = 1};
+        return new DeviceCalibration()
+        {
+            NoLoadSensorValue = DefaultParametersValue, 
+            NominalLoadSensorValue = DefaultParametersValue, 
+            NominalLoad = DefaultParametersValue
+        };
     }
 
     public bool IsValid()
     {
-        return NominalLoadSensorValue != DefaultParametersValue && NoLoadSensorValue != DefaultParametersValue;
+        return NominalLoadSensorValue != DefaultParametersValue 
+               && NoLoadSensorValue != DefaultParametersValue
+               && NominalLoad != DefaultParametersValue;
     }
 }
--- BleRecorder.UI.WPF/ViewModels/NavigationViewModel.cs
+++ BleRecorder.UI.WPF/ViewModels/NavigationViewModel.cs
@@ -107,10 +107,14 @@ namespace BleRecorder.UI.WPF.ViewModels
 
         private async Task ExportSelectedAsync()
         {
+            // TODO optimize query 
+            var reloadedSubjects = await _testSubjectRepository.GetAllWithRelatedDataAsync();
+
             var subjects = _navigationItems
                 .OfType<NavigationTestSubjectItemViewModel>()
                 .Where(item => item.IsSelected)
                 .Select(item => item.Model);
+                
             if (_fileManager.SaveSingleFileDialog("Export.xlsx", out var filePath))
             {
                 await Task.Run(() => _documentManager.Export(filePath!, subjects));
