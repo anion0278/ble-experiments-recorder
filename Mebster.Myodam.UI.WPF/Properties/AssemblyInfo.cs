973567caba3de66fd6ed7ce355f489ac53605738
Grushko Stefan <anion0278@gmail.com>
2022-10-18 20:38:56 +0200

    Updated version info

--- Mebster.Myodam.UI.WPF/Properties/AssemblyInfo.cs
+++ Mebster.Myodam.UI.WPF/Properties/AssemblyInfo.cs
@@ -15,19 +15,19 @@ using Microsoft.Extensions.Configuration.UserSecrets;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mebster")]
 [assembly: AssemblyProduct("Myodam GUI")]
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: UserSecretsId("6e5e5a67-1bf1-49d2-8de0-bb00e0048c73")]
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page,
-    // or application resource dictionaries)
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page,
-    // app, or any theme specific resource dictionaries)
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
 )]
 
 
